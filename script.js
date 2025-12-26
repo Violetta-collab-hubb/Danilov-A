ebeab5ac5b7f0638c75a1f13c790cb9f27c3b2e0
Violetta-collab-hubb <VMYakubova@kpfu.ru>
2025-12-26 16:11:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-function turnOn()  {
-            document.getElementById('bulboff').style.display = "none";   // Скрываем изображение OFF
-            document.getElementById('bulbon').style.display = "inline";  // Показываем изображение ON
-        }
-function turnOff() {
-            document.getElementById('bulboff').style.display = "inline";   // Показываем изображение OFF
-            document.getElementById('bulbon').style.display = "none";  // Скрываем изображение ON
-        }
+const choices = ['rock', 'paper', 'scissors'];
+let resultElement = document.querySelector('#result');
+
+// Генерируем случайный выбор компьютера
+function computerChoice() {
+    let randomIndex = Math.floor(Math.random() * choices.length); // получаем случайный индекс массива
+    return choices[randomIndex];
+}
+
+// Определяем победителя раунда
+function determineWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) return "Ничья!";
+
+    switch (playerSelection + '-' + computerSelection) {
+        case 'rock-scissors':
+        case 'paper-rock':
+        case 'scissors-paper':
+            return "Победил игрок!";
+        default:
+            return "Компьютер победил.";
+    }
+}
+
+// Обработчик кликов по кнопкам
+function play(playerSelection) {
+    let computerSelected = computerChoice(); // Получаем выбор компьютера
+    let winnerMessage = determineWinner(playerSelection, computerSelected); // Определяем победителя
+
+    // Показываем результат игроку
+    resultElement.textContent = `${winnerMessage}\nВаш выбор: ${playerSelection}, Компьютер выбрал: ${computerSelected}.`;
+}
