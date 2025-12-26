bbf6d9a7f47875ba3374d024cf78a184d02ad8bb
Violetta-collab-hubb <VMYakubova@kpfu.ru>
2025-12-26 16:08:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function turnOn()  {
+            document.getElementById('bulboff').style.display = "none";   // Скрываем изображение OFF
+            document.getElementById('bulbon').style.display = "inline";  // Показываем изображение ON
+        }
+function turnOff() {
+            document.getElementById('bulboff').style.display = "inline";   // Показываем изображение OFF
+            document.getElementById('bulbon').style.display = "none";  // Скрываем изображение ON
+        }
